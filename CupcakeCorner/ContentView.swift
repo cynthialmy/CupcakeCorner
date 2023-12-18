58de33dfa6509dd3200ac04cff01173d42d6d8ef
Mengyuan Li <mli114@volvocars.com>
2023-12-18 15:39:04 +0100

    added async image

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -36,6 +36,28 @@ struct ContentView: View {
     }
     
     var body: some View {
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder: {
+            ProgressView()
+        }
+        .frame(width: 200, height: 200)
+        
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
+            }
+        }
+        .frame(width: 200, height: 200)
+        
         List(results, id: \.trackId) {item in
             VStack(alignment: .leading) {
                 Text(item.trackName)
