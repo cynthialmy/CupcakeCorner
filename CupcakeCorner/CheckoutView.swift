e73d589461d7698600ff2dc4af29c41bfb4be368
Mengyuan Li <mli114@volvocars.com>
2023-12-20 14:36:04 +0100

    Checking for a valid address

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,20 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Cynthia LI on 2023-12-20.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView(order: Order())
+}
